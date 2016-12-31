578c70a03236175e42ad2d8c9f763a8a5ecd6c42
jpeck89 <james.peck.89@gmail.com>
2016-12-31 15:37:37 -0500

    updating generate script with flag options.
    changed default type of output to csv.
    Added flags for collecting key, schema output type, number of lines to retrieve, and positional argument for the file with the schema to produce from.
    
    replaced some hard coded variables to use argument provided to flags.

--- generator.py
+++ generator.py
@@ -1,22 +1,35 @@
 import requests
 import json
+import argparse
 
 """ This script uses Mockaroo API to generate an output json based on a schema passed to it.
 Official documentation site: https://www.mockaroo.com/api/docs
 """
 
+parser = argparse.ArgumentParser(description='Retrieve data from Mockaroo API')
+parser.add_argument('file',
+                    help='Configuratoin file to use')
+parser.add_argument('-k','--key', nargs=1,
+                    help='Enter the API key for Mockaroo')
+parser.add_argument('-n', '--number', nargs=1, default=5, type=int,
+                    help='Enter the number of records you would like returned')
+parser.add_argument('-s', '--schema', nargs=1, default=['csv'],
+                    choices=['json','csv','txt','custom','sql','xml'],
+                    help='Use one of the allowed data formats, if not spacified it will use CSV')
+args = parser.parse_args()
+
 # API URL .json at the end refers to the expected format of the output
-mockaroo_url = 'http://www.mockaroo.com/api/generate.json'
+mockaroo_url = 'http://www.mockaroo.com/api/generate.%s' % args.schema[0]
 # API key
-mockaroo_key = ''  # your mockaroo key
-num_rows_to_generate = 10  # Max 1000 for the free version of the API
+mockaroo_key = args.key  # your mockaroo key
+num_rows_to_generate = args.number  # Max 1000 for the free version of the API
 
 
 def post_request():
     # Generate the URL by appending key and count.
     # key is required parameter
     # if count is not given, default is one object is returned based on schema definition
-    url = url = mockaroo_url + '?key=' + mockaroo_key + '&count=' + str(num_rows_to_generate)
+    url = mockaroo_url + '?key=' + mockaroo_key + '&count=' + str(num_rows_to_generate)
 
     # schema.json refers to the schema definition of the output json to be generated
     # Refer official documentation for supported types and detailed explanation
