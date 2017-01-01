49123a923829b80f17a8958166b596af0a776fb1
jpeck89 <james.peck.89@gmail.com>
2017-01-01 18:09:30 -0500

    change the way arguments are given to requests object, update name of outfile
    Moved where the url is declared so it makes more sense
    added payload array to provide to requests object
    changed name of outfile so it matches returned schema

--- generator.py
+++ generator.py
@@ -9,6 +9,7 @@ Official documentation site: https://www.mockaroo.com/api/docs
 parser = argparse.ArgumentParser(description='Retrieve data from Mockaroo API')
 parser.add_argument('file',
                     help='Configuratoin file to use')
+# key is required parameter
 parser.add_argument('-k','--key', nargs=1,
                     help='Enter the API key for Mockaroo')
 parser.add_argument('-n', '--number', nargs=1, default=5, type=int,
@@ -18,31 +19,36 @@ parser.add_argument('-s', '--schema', nargs=1, default=['csv'],
                     help='Use one of the allowed data formats, if not spacified it will use CSV')
 args = parser.parse_args()
 
-# API URL .json at the end refers to the expected format of the output
-mockaroo_url = 'http://www.mockaroo.com/api/generate.%s' % args.schema[0]
 # API key
 mockaroo_key = args.key  # your mockaroo key
 num_rows_to_generate = args.number  # Max 1000 for the free version of the API
 
 
 def post_request():
-    # Generate the URL by appending key and count.
-    # key is required parameter
-    # if count is not given, default is one object is returned based on schema definition
-    url = mockaroo_url + '?key=' + mockaroo_key + '&count=' + str(num_rows_to_generate)
+    # Generate the URL by appending return format.
+    # API URL args.schema at the end refers to the expected format of the output
+    url = 'http://www.mockaroo.com/api/generate.%s' % args.schema[0]
 
     # schema.json refers to the schema definition of the output json to be generated
     # Refer official documentation for supported types and detailed explanation
 
     with open('schema.json', 'r') as datafile:
         fields = json.load(datafile)
-    # Make a post call to the URL
-    # Body of the post request is the schema definition of the output expected
-    response = requests.post(url=url, json=fields)
+        
+    # declare arguments required for post request to Mockaroo
+    payload = {
+            'fields':json.dumps(fields),
+            'key': args.key,
+            'count':args.number
+    }
 
-    # Write the output to a file (output.json)
+    # Make a post call to the URL
+    # Body of the post request is the schema definition of the output expected, API Key, and number of records to return
+    response = requests.post(url, params=payload)
+
+    # Write the output to a file ex. (output.json)
     with open('output.json', 'w') as outfile:
-        json.dump(response.json(), outfile)
+        outfile.write(response.text)
 
 
 if __name__ == '__main__':
