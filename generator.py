501869ddeaa0235723f9bea229b81f7e455bf0f5
James Peck <jpeck@ussignal.com>
2017-02-12 13:05:10 -0500

    Exception handling for bad JSON
    Edited file for better readability, as well as fixing old spelling mistakes.
    Made some exception handling in case bad JSON is provided.

--- generator.py
+++ generator.py
@@ -1,27 +1,30 @@
 import requests
 import json
 import argparse
+import sys
 
-""" This script uses Mockaroo API to generate an output json based on a schema passed to it.
+""" This script uses Mockaroo API to generate an output file based on a json schema passed to it.
 Official documentation site: https://www.mockaroo.com/api/docs
 """
 
 parser = argparse.ArgumentParser(description='Retrieve data from Mockaroo API')
+# file is required parameter
 parser.add_argument('file',
-                    help='Configuratoin file to use')
+                    help='REQUIRED: Configuratoin file to use')
 # key is required parameter
 parser.add_argument('-k','--key', nargs=1,
-                    help='Enter the API key for Mockaroo')
+                    help='REQUIRED: Enter the API key for Mockaroo')
 parser.add_argument('-n', '--number', nargs=1, default=5, type=int,
-                    help='Enter the number of records you would like returned')
+                    help='Enter the number of records you would like returned, the default is 5.')
 parser.add_argument('-s', '--schema', nargs=1, default=['csv'],
                     choices=['json','csv','txt','custom','sql','xml'],
-                    help='Use one of the allowed data formats, if not spacified it will use CSV')
+                    help='Use one of the allowed data formats, if not specified it will use CSV')
 args = parser.parse_args()
 
-# API key
-mockaroo_key = args.key  # your mockaroo key
+# convert args into easy to read variables
+mockaroo_key = args.key  # your mockaroo API key
 num_rows_to_generate = args.number  # Max 1000 for the free version of the API
+output_format = args.schema[0]
 
 
 def post_request():
@@ -32,9 +35,15 @@ def post_request():
     # schema.json refers to the schema definition of the output json to be generated
     # Refer official documentation for supported types and detailed explanation
 
-    with open(args.file, 'r') as datafile:
-        fields = json.load(datafile)
-        
+    try:
+      with open(args.file, 'r') as datafile:
+          fields = json.load(datafile)
+    except ValueError as error: #looking for errors in json loaded into script
+      print('There was an error with the JSON provided')
+      print(args.file)
+      print(error)
+      sys.exit(1)
+    
     # declare arguments required for post request to Mockaroo
     payload = {
             'fields':json.dumps(fields),
@@ -47,7 +56,7 @@ def post_request():
     response = requests.post(url, params=payload)
 
     # Write the output to a file ex. (output.json)
-    with open('output.%s' % args.schema[0], 'w') as outfile:
+    with open('output.%s' % output_format, 'w') as outfile:
         outfile.write(response.text)
 
 
